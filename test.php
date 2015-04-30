283a1b4f4f863544f13ba7b2fe1fc3f8ff31fe95
Jeroen Dalsem <jdalsem@coldtrick.com>
2015-04-30 10:18:42 +0200

    fixed class typo in button

--- test.php
+++ test.php
@@ -12,7 +12,7 @@ $body .= "<a href='/news'>/news</a><br/>";
 $body .= "<a href='#'>#</a><br/>";
 $body .= "<a href='javascript:void(0);'>javascript:void(0);</a><br/>";
 
-$body .= elgg_view("output/url", array("text" => "demo link as button", "class" => "elgg-button elgg-buttom-action", "href" => "http://google.nl"));
+$body .= elgg_view("output/url", array("text" => "demo link as button", "class" => "elgg-button elgg-button-action", "href" => "http://google.nl"));
 
 $content = elgg_view_layout("one_column", array("title" => elgg_echo("Test"), "content" => $body));
 
