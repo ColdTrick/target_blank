fae34a7b8e68e5a0c93a3f82bfbf848b8bdf565c
Jeroen Dalsem <jdalsem@coldtrick.com>
2018-12-14 16:41:49 +0100

    added: dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * This file was created by Translation Editor v7.0-beta
+ * On 2018-12-14 16:40
+ */
+
+return array (
+  'target_blank:settings:link_suffix' => 'Link Suffix',
+  'target_blank:settings:link_suffix:info' => 'Optioneel kun je een extra stukje HTML configureren wat achter elke externe link wordt toegevoegd. Hiermee kun je bijvoorbeeld een icoon toevoegen naast elke externe link om de indicatie te geven dat deze wordt geopend in een nieuw scherm.',
+);
