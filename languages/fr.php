57612eeaf6e4e64a96b21f697c6797702cc9cb18
Dan d'Auge <76149200+danhype@users.noreply.github.com>
2021-02-12 09:08:49 +0100

    Create fr.php
    for v 6.0.1

---
+++ languages/fr.php
@@ -0,0 +1,6 @@
+<?php
+
+return array (
+  'target_blank:settings:link_suffix' => 'Suffixe des liens',
+  'target_blank:settings:link_suffix:info' => 'Vous pouvez également saisir ici du code HTML qui sera ajouté à chaque lien externe. Vous pouvez utiliser cette option pour faire apparaître une icône à côté de chaque lien externe afin d\'indiquer qu\'il sera ouvert dans une nouvelle fenêtre.',
+);
